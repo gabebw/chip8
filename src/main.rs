89f62f31959bec7d230484d4cbb1251f2ec0fcc4
Gabe Berke-Williams <gabebw@gabebw.com>
2020-09-06 14:22:26 -0700

    1nnn: jump to a given address

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,7 @@ struct State {
 }
 
 impl State {
+    /// Create a new State with default values.
     fn new() -> Self {
         Self {
             memory: vec![0; 4096],
@@ -37,26 +38,51 @@ impl State {
         }
     }
 
+    /// Set the given register to the given value.
     fn set_register(&mut self, register: u8, value: u8) {
         self.registers[register as usize] = value;
     }
+
+    /// Set the program counter to the given address.
+    /// The address is a 12-bit nibble, meaning that its maximum value is 0x0FFF,
+    /// not 0xFFFF as the u16 type implies.
+    fn set_pc(&mut self, address: u16) {
+        assert!(address <= 0xFFF);
+        self.pc = address;
+    }
+}
+
+/// Given the value 0xABCD, return 0xBCD.
+/// A nibble is a 12-bit value.
+fn nibble(bytes: &u16) -> u16 {
+    bytes & 0x0FFF
 }
 
 fn run<'a>(
     state: &'a mut State,
     program: &[u16],
 ) -> Result<&'a mut State, Box<dyn std::error::Error>> {
-    for byte in program {
+    for chunk in program {
         // Convert a 2-byte value in the format 0xABCD into 0xA and 0xB
-        let a: u8 = (byte >> 12).try_into()?;
-        let b: u8 = (byte >> 8 & 0x000F).try_into()?;
+        let a: u8 = (chunk >> 12).try_into()?;
+        let b: u8 = (chunk >> 8 & 0x000F).try_into()?;
 
         match a {
+            // 1nnn - JP addr
+            // Jump to location nnn.
+            // The interpreter sets the program counter to nnn.
+            0x1 => {
+                let nibble = nibble(chunk);
+                state.set_pc(nibble);
+            }
+            // 6xkk - LD Vx, byte
+            // Set Vx = kk.
+            // The interpreter puts the value kk into register Vx.
             0x6 => {
-                let value: u8 = (byte & 0xFF).try_into()?;
+                let value: u8 = (chunk & 0xFF).try_into()?;
                 state.set_register(b, value);
             }
-            _ => panic!("Instruction not supported: {:x}", byte),
+            _ => panic!("Instruction not supported: {:x}", chunk),
         }
     }
 
@@ -70,8 +96,14 @@ mod test {
     use super::*;
 
     #[test]
-    // 6xkk - LD Vx, byte
-    // Set Vx = kk.
+    fn jp_addr() {
+        let mut state = State::new();
+        let program = vec![0x1BCD];
+        let new_state = run(&mut state, &program).unwrap();
+        assert_eq!(new_state.pc, 0xBCD);
+    }
+
+    #[test]
     fn ld_vx() {
         let mut state = State::new();
         let program = vec![0x6D12];
